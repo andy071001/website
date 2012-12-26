69ec367aa28d08d5b8d29809c599e866cd645f53
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2012-12-26 15:55:14 +0100

    Revamp news styles.

--- apps/news/menu.py
+++ apps/news/menu.py
@@ -37,14 +37,13 @@ class NewsMenu(CMSAttachMenu):
 	def get_nodes(self, request):
 		nodes = []
 
-		node = NavigationNode(_("Index"), "/news", 0)
+		node = NavigationNode(_("All News"), reverse("list_news"), 0)
 		nodes.append(node)
 
 		for item in News.pubs_objects.all()[:10]:
 			node = NavigationNode(
 				item.title,
-#				reverse("news_details", kwargs={"slug": item.slug}),
-				"/news/%s" % item.slug,
+				reverse("news_details", kwargs={"slug": item.slug}),
 				item.pk
 			)
 			nodes.append(node)
