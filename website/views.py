77091ac02d149e4269bcd08adb2d479dc81a149d
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2013-02-15 07:49:25 +0100

    Inline profile editing
    Use Bootstrap Editable for profile settings instead of a separate
    editing form.
    
    Closes: #16

--- website/views.py
+++ website/views.py
@@ -24,17 +24,20 @@
 # $END_LICENSE$
 ############################################################################
 
-from django.views.generic import TemplateView, FormView
+from django.views.generic import View, TemplateView, FormView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse_lazy
 
-from website.forms import EditProfileForm
-
-class ProtectedView(TemplateView):
+class ProtectedView(View):
 	@method_decorator(login_required)
 	def dispatch(self, *args, **kwargs):
 		return super(ProtectedView, self).dispatch(*args, **kwargs)
+
+class ProtectedTemplateView(TemplateView):
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(ProtectedTemplateView, self).dispatch(*args, **kwargs)
 
 class ProtectedFormView(FormView):
 	@method_decorator(login_required)
@@ -50,28 +53,11 @@ class PreviewView(TemplateView):
 class DiscoverView(TemplateView):
 	template_name = "pages/discover.html"
 
-class ProfileView(ProtectedView):
+class ProfileView(ProtectedTemplateView):
 	template_name = "registration/profile.html"
 
-class EditProfileView(ProtectedFormView):
-	template_name = "registration/profile.html"
-	http_method_names = ["get", "post"]
-	form_class = EditProfileForm
-	success_url = reverse_lazy("auth_profile")
-
-	def get_context_data(self, **kwargs):
-		context = super(EditProfileView, self).get_context_data(**kwargs)
-		context["edit_mode"] = True
-		return context
-
-	def get_initial(self):
-		return {
-			"first_name": self.request.user.first_name,
-			"last_name": self.request.user.last_name,
-			"date_of_birth": self.request.user.get_profile().date_of_birth,
-			"gender": self.request.user.get_profile().gender,
-			"receive_updates": self.request.user.get_profile().receive_updates
-		}
+class EditProfileView(ProtectedView):
+	http_method_names = ["post"]
 
 	def post(self, request, *args, **kwargs):
 		# Make sure the user is authenticated
@@ -79,20 +65,33 @@ class EditProfileView(ProtectedFormView):
 			from django.http import HttpResponseForbidden
 			return HttpResponseForbidden()
 
-		# Form
-		form_class = self.get_form_class()
-		form = self.get_form(form_class)
-		form.request = request
+		from django.utils import simplejson as json
+		from django.http import HttpResponse
 
-		if form.is_valid():
-			cleaned_data = form.clean()
-			request.user.first_name = cleaned_data["first_name"]
-			request.user.last_name = cleaned_data["last_name"]
-			request.user.get_profile().date_of_birth = cleaned_data["date_of_birth"]
-			request.user.get_profile().gender = cleaned_data["gender"]
-			request.user.get_profile().receive_updates = cleaned_data["receive_updates"]
+		pk = int(request.POST["pk"])
+		name = request.POST["name"]
+		value = request.POST["value"]
+		status = 200
+		response = None
+
+		try:
+			if name == "first_name":
+				request.user.first_name = value
+			elif name == "last_name":
+				request.user.last_name = value
+			elif name == "date_of_birth":
+				request.user.get_profile().date_of_birth = value
+			elif name == "gender":
+				request.user.get_profile().gender = value
+			elif name == "receive_updates":
+				request.user.get_profile().receive_updates = True if value == "true" else False
+			else:
+				raise Exception("%s is an unknown field" % name)
+		except Exception, e:
+			response = e.message
+			status = 500
+		else:
 			request.user.save()
 			request.user.get_profile().save()
-			return self.form_valid(form)
-		else:
-			return self.form_invalid(form)
+
+		return HttpResponse(response, status=status, mimetype="text/plain")
